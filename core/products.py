735e06722e9541199f9de854a588b3309a68427d
Vladimir <darbeka.vova2@mail.ru>
2025-10-22 15:03:13 +0700

    feat(core): improve build() with scalable None-check and empty name validation

--- core/products.py
+++ core/products.py
@@ -1,11 +1,24 @@
-def build(name: str, calories: float, proteins: float, carbs: float, fats: float) -> dict:
-    if name is None or calories is None or proteins is None or carbs is None or fats is None:
-        raise ValueError("error: data null")
-    else:
-        return {
-            "name_product": name,
-            "product_calories": calories,
-            "product_proteins": proteins,
-            "product_carbs": carbs,
-            "product_fats": fats
-        }
+def build(
+        name: str,
+        calories: float,
+        proteins: float,
+        carbs: float,
+        fats: float
+    ) -> dict:
+
+    values_name = ["name", "calories", "proteins", "carbs", "fats"]
+    values = [name, calories, proteins, carbs, fats]
+    for value_name, value in zip(values_name, values):
+        if value is None:
+            raise ValueError(f"error: {value_name} is None")
+        
+    if name.strip() == "":
+        raise ValueError(f"error: name is empty")
+    
+    return {
+        "name_product": name,
+        "product_calories": calories,
+        "product_proteins": proteins,
+        "product_carbs": carbs,
+        "product_fats": fats
+    }
